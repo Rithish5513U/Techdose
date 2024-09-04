b56f30394d59a0162ccc550fd9d593c659cba06e
Rithish S <rithish.s2022cce@sece.ac.in>
2024-09-04 23:28:31 +0530

    update

--- segmentedSieve.cpp
+++ segmentedSieve.cpp
@@ -5,21 +5,25 @@ vector<int> segmentedSeive(int left, int right){
     vector<bool> primes(sqrt(right)+1, true);
     for(int i=2; i*i<=right; i++){
         if(primes[i]){
-            for(int j=i*i; j<=right; j+=i)
+            for(int j=i*i; j*j<=right; j+=i)
                 primes[j] = false;
         }
     }
-    vector<bool> primesInRange(right - left + 1, true);
+    vector<int> prime;
     for(int i=2; i*i<=right; i++){
-        if(primes[i]){
-            for(int j = ceil(left/i); j*i<=right; j++){
-                primesInRange[(j*i)-left] = false;
-            }
+        if(primes[i])   prime.push_back(i);
+    }
+    vector<bool> primesInRange(right-left+1, true);
+    for(int i : prime){
+        for(int j=left/i; j*i<=right; j++){
+            if(j==1)    continue;
+            primesInRange[j*i-left] = false;
         }
     }
     vector<int> ans;
     for(int i=0; i<primesInRange.size(); i++){
         if(primesInRange[i]){
+            if(i+left==1)   continue;
             ans.push_back(i+left);
         }
     }
